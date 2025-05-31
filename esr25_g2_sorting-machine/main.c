0c7cd893c52b11d29cc11919c865317cac99e7e7
Luis Wehrberger <luis.wehrberger2@gmail.com>
2025-05-31 14:51:50 +0200

    Add main implementation for turning servo - inits I2C - inits the PCA - added delay helpers temporarliy - unlock all GPIOS - turns servo at channel 0 between 0 and 180 deg with 2 sec delay between - cssproject and cproject changed automatically

--- esr25_g2_sorting-machine/main.c
+++ esr25_g2_sorting-machine/main.c
@@ -1,12 +1,40 @@
 #include <msp430.h> 
+#include "PCA9685.h"
+#include "I2C.h"
+#include <stdint.h>
 
+// TODO: Replace with timers or remove if not needed
+void delay_ms(uint16_t ms) {
+    while (ms--) {
+        __delay_cycles(1000); // Assuming 1 MHz clock, 1000 cycles = 1 ms
+    }
+}
+
+// TODO: Replace with timers or remove if not needed
+void delay_s(uint16_t seconds) {
+    while (seconds--) {
+        delay_ms(1000); // 1000 ms = 1 second
+    }
+}
 
 /**
  * main.c
  */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+	WDTCTL = WDTPW | WDTHOLD;
+    PM5CTL0 &= ~LOCKLPM5;  
+
+    init_I2C();
+	init_PCA9685();
+
+	while (1) {
+		set_servo_angle(0, 0);
+
+		delay_ms(2000);
+
+		set_servo_angle(0, 180);
+
+		delay_ms(2000);
+    }
 }
