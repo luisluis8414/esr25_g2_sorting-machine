1952cb0b42ed802b368346e44f3be0e7976a620e
Luis Wehrberger <luis.wehrberger2@gmail.com>
2025-06-29 11:37:56 +0200

    add: button doxygen comments

--- esr25_g2_sorting-machine/button/button.c
+++ esr25_g2_sorting-machine/button/button.c
@@ -1,8 +1,16 @@
-/*
- * button.c
+/* ========================================================================== */
+/* button.c                                                                      */
+/* ========================================================================== */
+/**
+ * @file      button.c
+ * @author    raachl
+ * @date      22.06.2025
  *
- *  Created on: 22.06.2025
- *      Author: raachl
+ * @brief     Button Implementierung für die Sortieranlage.
+ *
+ * Dieses Modul implementiert die ISRs für die Buttons
+ * und das Debouncing mittels Timer. Die Buttons generieren Events für die
+ * State Machine wenn sie gedrückt werden.
  */
 
 #include "button.h"
@@ -12,8 +20,18 @@
 
 extern Event_t eventBits;
 
+/** @brief Status Flag für aktives Debouncing */
 static volatile bool debounce_active = false;
 
+/**
+ * @brief Initialisiert die Button Hardware.
+ *
+ * Konfiguriert die GPIO Pins für beide Buttons:
+ *   - P4.1 (Button 1): Input mit Pull-up und Interrupt
+ *   - P2.3 (Button 2): Input mit Pull-up und Interrupt
+ *
+ * Bereitet auch Timer B2 für das Debouncing vor.
+ */
 void button_init(void)
 {
     // --- P4.1 (Button1) konfigurieren ---
@@ -22,7 +40,7 @@ void button_init(void)
     P4REN |= BIT1;
     P4IES |= BIT1;
     P4IFG &= ~BIT1;
-    P4IE  |= BIT1;
+    P4IE |= BIT1;
 
     // --- P2.3 (Button2) konfigurieren ---
     P2DIR &= ~BIT3;
@@ -30,16 +48,24 @@ void button_init(void)
     P2REN |= BIT3;
     P2IES |= BIT3;
     P2IFG &= ~BIT3;
-    P2IE  |= BIT3;
+    P2IE |= BIT3;
 
-    // Timer_B2 vorbereiten (für ACLK-Takt)
+    // Timer B2 vorbereiten (für ACLK Takt)
     TB2CTL = TBSSEL__ACLK | MC__STOP | TBCLR;
     TB2CCTL0 = 0;
 }
 
+/**
+ * @brief Startet den Debounce Timer.
+ *
+ * Startet Timer B2 für 500ms Debouncing Zeit und deaktiviert
+ * die Button Interrupts während dieser Zeit. Verhindert mehrfaches
+ * Starten des Timers.
+ */
 inline void button_debounce_start(void)
 {
-    if (debounce_active) return;
+    if (debounce_active)
+        return;
 
     debounce_active = true;
 
@@ -51,31 +77,49 @@ inline void button_debounce_start(void)
     P2IE &= ~BIT3;
 }
 
-// ISR für Port 4
+/**
+ * @brief Interrupt Service Routine für Port 4 (Button 1).
+ *
+ * Wird aufgerufen wenn Button 1 gedrückt wird. Setzt das entsprechende
+ * Event Bit und startet das Debouncing.
+ */
 #pragma vector = PORT4_VECTOR
 __interrupt void Port_4_ISR(void)
 {
-    if (!debounce_active) {
+    if (!debounce_active)
+    {
         eventBits |= EVT_S1;
         button_debounce_start();
         _bic_SR_register_on_exit(LPM3_bits);
     }
-        P4IFG &= ~BIT1;
+    P4IFG &= ~BIT1;
 }
 
-// ISR für Port 2
+/**
+ * @brief Interrupt Service Routine für Port 2 (Button 2).
+ *
+ * Wird aufgerufen wenn Button 2 gedrückt wird. Setzt das entsprechende
+ * Event Bit und startet das Debouncing.
+ */
 #pragma vector = PORT2_VECTOR
 __interrupt void Port_2_ISR(void)
 {
-    if (!debounce_active) {
+    if (!debounce_active)
+    {
         eventBits |= EVT_S2;
         button_debounce_start();
         _bic_SR_register_on_exit(LPM3_bits);
     }
-        P2IFG &= ~BIT3;
+    P2IFG &= ~BIT3;
 }
 
-// ISR für Timer_B2 CCR0
+/**
+ * @brief Timer B2 Interrupt Service Routine für Debouncing.
+ *
+ * Wird nach 500ms aufgerufen um das Debouncing zu beenden.
+ * Stoppt den Timer, deaktiviert den Timer Interrupt und
+ * reaktiviert die Button Interrupts.
+ */
 #pragma vector = TIMER2_B0_VECTOR
 __interrupt void TimerB2_ISR(void)
 {
@@ -85,7 +129,7 @@ __interrupt void TimerB2_ISR(void)
     debounce_active = false;
 
     P4IFG &= ~BIT1;
-    P4IE  |= BIT1;
+    P4IE |= BIT1;
     P2IFG &= ~BIT3;
-    P2IE  |= BIT3;
+    P2IE |= BIT3;
 }
